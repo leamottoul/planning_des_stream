a84ad52ca6efb35c72b431403e728af5df2ef871
leamottoul <leamottoul@gmail.com>
2025-04-26 14:45:19 +0200

    mage V2

---
+++ users.js
@@ -0,0 +1,22 @@
+const users = [
+  { 
+    username: "admin",
+    password: "f66218a3ce36c620f949e46c97bd66f27867c8cf2bf701762c83e21ca0ff87bf",
+    role: "admin"
+  },
+  {
+    username: "leatv",
+    password: "9b04e2a1864843df1483760e33b17e973bd828515ecb99980005b81ed989ba39",
+    role: "leatv"
+  },
+  {
+    username: "modoco",
+    password: "356e7515307811eb83f4a8ad448baff0dd2ede9f9509ca77e8591f106b97bee6",
+    role: "modo"
+  },
+  {
+    username: "modo__",
+    password: "92925488b28ab12584ac8fcaa8a27a0f497b2c62940c8f4fbc8ef19ebc87c43e",
+    role: "modo"
+  },
+]
